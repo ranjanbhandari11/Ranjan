ed232bab1beed481898aaa352043a91bd9f8601d
Ranjan Bhandari <ranjanbhandari11@gmail.com>
2026-05-11 19:03:51 +1000

    Remove Render runtime hotfix layer

--- render-hotfix.remote.js
+++
@@ -1,4 +0,0 @@
-const message =
-  "PantryPal render-hotfix.remote: legacy remote patching disabled; checked-in assets remain unchanged.";
-
-console.log(message);
--- render-patch.js
+++
@@ -1,4 +0,0 @@
-const message =
-  "PantryPal render-patch: deploy patching disabled; using repository files without runtime rewrites.";
-
-console.log(message);
